cca63a6104f0054c871e85fee5e0004f010803cd
Renate <143175623+renayoo@users.noreply.github.com>
2024-10-20 18:26:11 +0200

    Husky commit hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,25 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
+
+  // Custom configuration
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': ['error'],
+    },
+  },
 ];
